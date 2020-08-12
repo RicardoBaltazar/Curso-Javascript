d6e46c90246a498641da946daf6fa57f01e758e2
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-12 18:44:30 -0300

    banner

--- take-away/src/components/header/index.jsx
+++ take-away/src/components/header/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './menu.css'
 
+const img = require('../../assets/imgIlustration.png')
+
 export default function Menu(){
     return (
+        <>
         <header>
             <nav className='menu'>
                 <h1>Take Away</h1>
                 <a href="">Sobre</a>
                 <a href="">Receitas</a>
             </nav>
+            <div className='banner'>
+                <div>
+                    <h2>As melhores receitas</h2>
+                <p>
+                Esta com fome? escolha um de nossos <br/>
+                melhores pratos e faça seu pedido! <br/>
+                Temos uma variedade de pratos! <br/>
+                </p>
+                </div>
+                <img src={img} alt="iamgen chefe de cozinha" className='image-banner'/>
+            </div>
         </header>
+        </>
     )
 }
