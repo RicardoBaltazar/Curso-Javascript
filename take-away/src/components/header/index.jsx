ce1379fbabc76c3595fea4c8afcdcb41022ba093
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-12 17:18:59 -0300

    menu

---
+++ take-away/src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './menu.css'
+
+export default function Menu(){
+    return (
+        <header>
+            <nav className='menu'>
+                <h1>Take Away</h1>
+                <a href="">Sobre</a>
+                <a href="">Receitas</a>
+            </nav>
+        </header>
+    )
+}
