c74a444501c86aa2f2d3a3e18f99b1a13bfbe48f
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-08-13 13:29:24 -0300

    imagens teste adicionadas

--- take-away/src/components/main/index.jsx
+++ take-away/src/components/main/index.jsx
@@ -3,18 +3,23 @@ import './main.css'
 
 import Cards from '../cards/'
 
-const image = require('../../assets/prato1.jpg')
+const image1 = require('../../assets/prato1.jpg')
+const image2 = require('../../assets/img2.jpg')
+const image3 = require('../../assets/img3.jpg')
+const image4 = require('../../assets/Spinach-&-Ricotta-Cannelloni.jpg')
+const image5 = require('../../assets/caldeirada.jpg')
+const image6 = require('../../assets/frango.jpg')
 
 export default function Main() {
     return (
         <>
             <div className='main'>
-                <Cards img={image} title="prato do dia"/>
-                <Cards img={image} title="prato do dia"/>
-                <Cards img={image} title="prato do dia"/>
-                <Cards img={image} title="prato do dia"/>
-                <Cards img={image} title="prato do dia"/>
-                <Cards img={image} title="prato do dia"/>
+                <Cards img={image1} title="Torta de presunto"/>
+                <Cards img={image2} title="Torta de abóbora"/>
+                <Cards img={image3} title="Sopa de cordeiro tunisina"/>
+                <Cards img={image4} title="Canelone de espinafre e ricota"/>
+                <Cards img={image5} title="Caldeirada de Peixe com Rouille"/>
+                <Cards img={image6} title="Cassarola de frango enchilado"/>
             </div>
         </>
     )
