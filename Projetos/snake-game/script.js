3e203ec4db23f1279815976e1c557afedb7546ad
unknown <ricardo.baltazar04@gmail.com>
2021-03-08 19:46:38 -0300

    projeto snake game parte 2

--- Projetos/snake-game/script.js
+++ Projetos/snake-game/script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8  * box,
     y: 8 * box
 }
+let direction = 'right';
 
 function createBG(){
     context.fillStyle = 'lightgreen';
@@ -19,5 +20,27 @@ function createSnake(){
     }
 }
 
-createBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX = snakeX + box;
+    if(direction == 'left') snakeX = snakeX - box;
+    if(direction == 'up') snakeY = snakeY - box;
+    if(direction == 'down') snakeY = snakeY + box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+}
+
+let game = setInterval(startGame, 100);
